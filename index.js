068ee1a4fa4fe11f0fa204ba5da98db1c43a22e9
iykesworld <kennethiyke12@gmail.com>
2025-01-19 02:33:08 +0000

    added frontend url to cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // parse options
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://travel-blog-frontend-257z.onrender.com"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
